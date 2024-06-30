4b3f1a0b8714b7a97e40ec12cf94cc65f2fbf5f2
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-06-30 10:31:21 +0300

    add button

--- src/features/search/ui/search.tsx
+++ src/features/search/ui/search.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@nextui-org/react';
+import { Input, Tooltip } from '@nextui-org/react';
 import { IoSearchOutline } from 'react-icons/io5';
 
 export function Search() {
@@ -8,7 +8,11 @@ export function Search() {
       radius="lg"
       placeholder="Поиск..."
       startContent={
-        <IoSearchOutline className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+        <Tooltip content={'Найти'}>
+          <button>
+            <IoSearchOutline className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+          </button>
+        </Tooltip>
       }
     />
   );
