cb77b358133a1953bf1cb9c12962b0513ffaf794
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-07-11 10:47:05 +0300

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false,
-  optimizeFonts: false,
-  swcMinify: true,
   images: {
     domains: ['irecommend.storage.yandexcloud.net'],
   },
